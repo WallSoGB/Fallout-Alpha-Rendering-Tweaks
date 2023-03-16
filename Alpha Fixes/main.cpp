ea4060033d03073b80fe922fc5661eb9eda63188
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2023-03-16 23:19:09 +0100

    Version bump

--- Alpha Fixes/main.cpp
+++ Alpha Fixes/main.cpp
@@ -15,7 +15,7 @@ bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInfo* info)
 {
 	info->infoVersion = PluginInfo::kInfoVersion;
 	info->name = "Fallout Alpha Rendering Tweaks";
-	info->version = 2;
+	info->version = 210;
 	return true;
 }
 
