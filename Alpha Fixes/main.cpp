c359f2c875f50f231e83ac2101658d961e8b096a
WallSoGB <11296448+WallSoGB@users.noreply.github.com>
2023-04-20 23:44:36 +0200

    Add INI

--- Alpha Fixes/main.cpp
+++ Alpha Fixes/main.cpp
@@ -171,28 +171,24 @@ namespace TMSAA {
 				// Check for "No_Transparency_Multisampling"
 				if (bCandidatePass && pCurrentPass->pGeometry) {
 					const NiGeometry* pGeo = pCurrentPass->pGeometry;
-					if (pGeo) {
-						const BSShaderProperty* shaderProp = (BSShaderProperty*)pGeo->shaderProperties.m_shadeProperty;
-						if (shaderProp) {
+					const BSShaderProperty* shaderProp = (BSShaderProperty*)pGeo->shaderProperties.m_shadeProperty;
+					if (pGeo && shaderProp) {
 #if _DEBUG
-							_MESSAGE("[SetTMSAAState] [TMSAA check] Current pass: %i | %s | %s", passType, BSRenderPass::GetPassName(passType), BSRenderPass::GetCurrentPassName());
-							NiShadeProperty::ShaderType eShaderType = shaderProp->m_eShaderType;
-							_MESSAGE("[SetTMSAAState] [TMSAA check] Shader type: %s", NiShadeProperty::GetShaderType(eShaderType));
-							if (eShaderType != NiShadeProperty::kType_Sky && eShaderType != NiShadeProperty::kType_TallGrass) {
-								const char* model = GetModelPath(FindReferenceFor3D((NiNode*)pGeo));
-								_MESSAGE("[SetTMSAAState] [TMSAA check] Current geo: %s, %s", pGeo->m_pkParent->m_kName, model ? model : "Unknown");
-							}
+						_MESSAGE("[SetTMSAAState] [TMSAA check] Current pass: %i | %s | %s", passType, BSRenderPass::GetPassName(passType), BSRenderPass::GetCurrentPassName());
+						NiShadeProperty::ShaderType eShaderType = shaderProp->m_eShaderType;
+						_MESSAGE("[SetTMSAAState] [TMSAA check] Shader type: %s", NiShadeProperty::GetShaderType(eShaderType));
+						if (eShaderType != NiShadeProperty::kType_Sky && eShaderType != NiShadeProperty::kType_TallGrass) {
+							const char* model = GetModelPath(FindReferenceFor3D((NiNode*)pGeo));
+							_MESSAGE("[SetTMSAAState] [TMSAA check] Current geo: %s, %s", pGeo->m_pkParent->m_kName, model ? model : "Unknown");
+						}
 #endif
-							if (shaderProp->m_eShaderType != -1) {
-								if ((shaderProp->BSShaderFlags[1] & BSShaderProperty::kFlags2_No_Transparency_Multisampling) != 0) {
+						if (shaderProp->m_eShaderType != -1 && (shaderProp->BSShaderFlags[1] & BSShaderProperty::kFlags2_No_Transparency_Multisampling) != 0) {
 #if _DEBUG
-									_MESSAGE("[SetTMSAAState] [Default] [TMSAA check] Disabling...");
+							_MESSAGE("[SetTMSAAState] [Default] [TMSAA check] Disabling...");
 #endif
-									eCustomState = DISABLE;
-									bEnable = 0;
-									break;
-								}
-							}
+							eCustomState = DISABLE;
+							bEnable = 0;
+							break;
 						}
 					}
 				}
@@ -266,6 +262,24 @@ namespace TMSAA {
 		SetState(currentStatus.bEnable, currentStatus.bMarkStatus, apRenderpass);
 		CdeclCall(0xB994F0, apRenderpass, uicurrentPass, bTestAlpha, bBlendAlpha, a4);
 	}
+
+	// Thx karut for help with jumping
+	// Fixes a flag check for Transparency Multisampling to check for both blending and testing values
+	// Vanilla simply goes "Does mesh have testing enabled?", "Yes?", "Great! Now it will be forcibly using TMSAA breaking your stuff because it's meant for alpha blending"
+	__declspec (naked) void AlphaMalding() {
+		static const uintptr_t kDisable = 0xB99568;
+		static const uintptr_t kEnable = 0xB99562;
+		__asm {
+			mov ax, [eax + 0x18]
+			test ax, 0x200 // Alpha Test... test
+			jz itsOver
+			test ax, 1 // Alpha Blend test
+			jz itsOver
+			jmp kEnable
+			itsOver :
+			jmp kDisable
+		}
+	}
 }
 
 // This one's cool.
@@ -296,55 +310,52 @@ void __fastcall SetBlendAlpha(BSShader* thiss, void*, NiGeometry::ShaderProperti
 	}
 }
 
-// Thx karut for help with jumping
-// Fixes a flag check for Transparency Multisampling to check for both blending and testing values
-// Vanilla simply goes "Does mesh have testing enabled?", "Yes?", "Great! Now it will be forcibly using TMSAA breaking your stuff because it's meant for alpha blending"
-__declspec (naked) void AlphaMalding() {
-	static const uintptr_t kDisable = 0xB99568;
-	static const uintptr_t kEnable = 0xB99562;
-	__asm {
-		mov ax, [eax + 0x18]
-		test ax, 0x200 // Alpha Test... test
-		jz itsOver
-		test ax, 1 // Alpha Blend test
-		jz itsOver
-		jmp kEnable
-		itsOver :
-		jmp kDisable
-	}
-}
-
 bool NVSEPlugin_Load(NVSEInterface* nvse) {
+	char iniDir[MAX_PATH];
+	GetModuleFileNameA(GetModuleHandle(NULL), iniDir, MAX_PATH);
+	strcpy((char*)(strrchr(iniDir, '\\') + 1), "Data\\NVSE\\Plugins\\FART.ini");
+	bool bForceTMSAA = GetPrivateProfileInt("Main", "bForceTMSAA", 1, iniDir);
+	bool bAlphaBlendFix = GetPrivateProfileInt("Main", "bAlphaBlendFix", 1, iniDir);
+	bool bTMSAAPatches = GetPrivateProfileInt("Main", "bTMSAAPatches", 1, iniDir);
+
 	if (!nvse->isEditor) {
-		for (UInt32 callAddr : { 0x10AF378, 0x10AF7E0, 0x10AFA60, 0x10AFE98, 0x10B0040, 0x10B0218, 0x10B03B8, 0x10B0598, 0x10B0740, 0x10B08E0, 0x10B0A80, 0x10B0C18, 0x10B0DC0, 0x10B0F40, 0x10B10C0, 0x10B1240, 0x10B13C0, 0x10B1540, 0x10B16C0, 0x10B1878, 0x10B1A00, 0x10B1B88, 0x10B1D10, 0x10B1EA0, 0x10B2030, 0x10B21C0, 0x10B2388, 0x10B2510, 0x10B2690, 0x10B2828, 0x10B29A0, 0x10B2B18, 0x10B2C90, 0x10B2E30, 0x10B2FD0, 0x10B3160, 0x10B32E8, 0x10B3470, 0x10B3618, 0x10B3828, 0x10B39D8, 0x10B3B70, 0x10B3CF8, 0x10B3E98, 0x10B4028, 0x10B41B8, 0x10B4348, 0x10B44E0, 0x10B4680, 0x10B4820, 0x10B49B8, 0x10B4B50, 0x10B4CE8, 0x10B4E68, 0x10B5000, 0x10B51C0, 0x10B5340, 0x10B5500, 0x10B56C0, 0x10B5850, 0x10B59F8, 0x10B5B90, 0x10B5D38, 0x10B5F28, 0x10B60C8, 0x10B6270, 0x10B6418, 0x10B65C8, 0x10B6770, 0x10B6920, 0x10B6AC0, 0x10B6C58, 0x10B6E10, 0x10B6FB8, 0x10B7158, 0x10B72F8, 0x10B7498, 0x10B7618, 0x10B77A0, 0x10B7938, 0x10B7AD0, 0x10B7C70, 0x10B8A00, 0x10B8F00, 0x10B9210, 0x10B9B28, 0x10BA430, 0x10BAE18, 0x10BAFE0, 0x10BB490, 0x10BB608, 0x10BB828, 0x10BBA00, 0x10BBBD0, 0x10BBD88, 0x10BBF38, 0x10BC0F0, 0x10BC2E0, 0x10BC698, 0x10BC978, })
-		{
-			SafeWrite32(callAddr, UInt32(SetBlendAlpha));
-		}
-		WriteRelJump(0xBBC5FD, SetBlendAlpha);
-		WriteRelCall(0xBC8F0B, SetBlendAlpha);
-
-		WriteRelJump(0xB99556, (UInt32)AlphaMalding);
-
-		// TMSAA
-		for (UInt32 callAddr : { 0x714C54, 0xB9956C, 0xB9A37E, 0xB9A38A}) {
-			WriteRelCall(callAddr, TMSAA::SetStateEx);
+		if (bAlphaBlendFix) {
+			for (UInt32 callAddr : { 0x10AF378, 0x10AF7E0, 0x10AFA60, 0x10AFE98, 0x10B0040, 0x10B0218, 0x10B03B8, 0x10B0598, 0x10B0740, 0x10B08E0, 0x10B0A80, 0x10B0C18, 0x10B0DC0, 0x10B0F40, 0x10B10C0, 0x10B1240, 0x10B13C0, 0x10B1540, 0x10B16C0, 0x10B1878, 0x10B1A00, 0x10B1B88, 0x10B1D10, 0x10B1EA0, 0x10B2030, 0x10B21C0, 0x10B2388, 0x10B2510, 0x10B2690, 0x10B2828, 0x10B29A0, 0x10B2B18, 0x10B2C90, 0x10B2E30, 0x10B2FD0, 0x10B3160, 0x10B32E8, 0x10B3470, 0x10B3618, 0x10B3828, 0x10B39D8, 0x10B3B70, 0x10B3CF8, 0x10B3E98, 0x10B4028, 0x10B41B8, 0x10B4348, 0x10B44E0, 0x10B4680, 0x10B4820, 0x10B49B8, 0x10B4B50, 0x10B4CE8, 0x10B4E68, 0x10B5000, 0x10B51C0, 0x10B5340, 0x10B5500, 0x10B56C0, 0x10B5850, 0x10B59F8, 0x10B5B90, 0x10B5D38, 0x10B5F28, 0x10B60C8, 0x10B6270, 0x10B6418, 0x10B65C8, 0x10B6770, 0x10B6920, 0x10B6AC0, 0x10B6C58, 0x10B6E10, 0x10B6FB8, 0x10B7158, 0x10B72F8, 0x10B7498, 0x10B7618, 0x10B77A0, 0x10B7938, 0x10B7AD0, 0x10B7C70, 0x10B8A00, 0x10B8F00, 0x10B9210, 0x10B9B28, 0x10BA430, 0x10BAE18, 0x10BAFE0, 0x10BB490, 0x10BB608, 0x10BB828, 0x10BBA00, 0x10BBBD0, 0x10BBD88, 0x10BBF38, 0x10BC0F0, 0x10BC2E0, 0x10BC698, 0x10BC978 })
+			{
+				SafeWrite32(callAddr, UInt32(SetBlendAlpha));
+			}
+			WriteRelJump(0xBBC5FD, SetBlendAlpha);
+			WriteRelCall(0xBC8F0B, SetBlendAlpha);
 		}
 
-		for (UInt32 callAddr : {0xB99FAF, 0xB9A004, 0xB9A00E, 0xB9A187, 0xB9A1AB, 0xB9A1E0, 0xB9A215, 0xB9A24A }) {
-			WriteRelCall(callAddr, TMSAA::CollectState);
+		if (bTMSAAPatches) {
+			WriteRelJump(0xB99556, (UInt32)TMSAA::AlphaMalding);
+
+			// TMSAA
+			for (UInt32 callAddr : { 0x714C54, 0xB9956C, 0xB9A37E, 0xB9A38A }) {
+				WriteRelCall(callAddr, TMSAA::SetStateEx);
+			}
+
+			for (UInt32 callAddr : {0xB99FAF, 0xB9A004, 0xB9A00E, 0xB9A187, 0xB9A1AB, 0xB9A1E0, 0xB9A215, 0xB9A24A }) {
+				WriteRelCall(callAddr, TMSAA::CollectState);
+			}
+
+			for (UInt32 callAddr : {0xB9A2A1, 0xB99FE4 }) {
+				WriteRelCall(callAddr, TMSAA::RenderPassImmediately);
+			}
 		}
 
-		for (UInt32 callAddr : {0xB9A2A1, 0xB99FE4 }) {
-			WriteRelCall(callAddr, TMSAA::RenderPassImmediately);
+		if (bForceTMSAA) {
+			WriteRelCall(0x4DB187, (UInt32)SetShaderPackageHook);
+			WriteRelCall(0x4DCB9D, (UInt32)SetShaderPackageHook);
 		}
-
-		WriteRelCall(0x4DB187, (UInt32)SetShaderPackageHook);
-		WriteRelCall(0x4DCB9D, (UInt32)SetShaderPackageHook);
 	}
 	else {
 		bIsGeck = 1;
-		WriteRelCall(0x429D5B, SetShaderPackageHook);
-		WriteRelCall(0x53198B, SetShaderPackageHook);
+		if (bForceTMSAA) {
+			WriteRelCall(0x429D5B, SetShaderPackageHook);
+			WriteRelCall(0x53198B, SetShaderPackageHook);
+		}
 	}
 
 	return true;
